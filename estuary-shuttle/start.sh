29cc7c25202d1cd4dedc0b3a869d546372ea5faa
mike seiler <snissn@gmail.com>
2022-10-06 08:49:46 -1000

    docker shuttle didnt start without this ld library path file

--- estuary-shuttle/start.sh
+++ estuary-shuttle/start.sh
@@ -11,5 +11,6 @@ echo "Shuttle Token: $ESTUARY_SHUTTLE_TOKEN"
 echo "Shuttle Handle: $ESTUARY_SHUTTLE_HANDLE"
 echo "Estuary Token: $ESTUARY_TOKEN"
 
+export LD_LIBRARY_PATH=/usr/lib/aarch64-linux-gnu/
 /usr/estuary-bin/estuary-shuttle --dev --estuary-api=$ESTUARY_HOSTNAME --auth-token=$ESTUARY_SHUTTLE_TOKEN --handle=$ESTUARY_SHUTTLE_HANDLE
 # tail -f /dev/null
